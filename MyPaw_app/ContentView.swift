86e480732ee73608cd824480276c7e3d6d625581
annalisapacchiarotti <annalisa.pacchiarotti@ied.edu>
2020-03-23 12:07:22 +0100

    Added pod Alamofire

--- MyPaw_app/ContentView.swift
+++ MyPaw_app/ContentView.swift
@@ -6,16 +6,38 @@
 //  Copyright © 2020 Annalisa Pacchiarotti. All rights reserved.
 //
 
-import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World 111111 nkbjkjhgjlhgljhgluihj!")
-    }
+import SwiftUI
+import Alamofire
+
+ struct Login: Encodable {
+    let email: String
+    let password: String
 }
 
+struct ContentView: View {
+
+    let login = Login(email: "annalisa.pacchiarotti@gmail.com", password: "lalalala")
+
+    var body: some View {
+
+        Button(action: {
+            AF.request("http://127.0.0.1:8000/api/login",
+                   method: .post,
+                   parameters: self.login,
+                   encoder: JSONParameterEncoder.default).response { response in
+            debugPrint(response)
+            }
+
+        }) {
+            Text("Login")
+
+        }
+    }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '13.2'
+
+target 'MyPaw_app' do
+  
+  pod 'Alamofire', '~> 5.0'
+
+end
